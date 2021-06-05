a6a282dbadb1462d35d847866efce3fecc4ab284
Abner Silva <abner.computadores@outlook.com>
2021-06-05 18:00:31 -0300

    retornando navegação de search para home

--- app/src/main/java/br/com/altechno/sbmovies/ui/search/SearchFragment.kt
+++ app/src/main/java/br/com/altechno/sbmovies/ui/search/SearchFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.navigation.fragment.findNavController
 import br.com.altechno.sbmovies.R
 
 class SearchFragment : Fragment() {
@@ -15,5 +17,15 @@ class SearchFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_search, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val backButton: ImageView = view.findViewById(R.id.back_button)
+
+        backButton.setOnClickListener {
+            findNavController().navigateUp()
+        }
     }
 }
