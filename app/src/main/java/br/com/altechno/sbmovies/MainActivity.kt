6dbb8700dc2b381e4b311469d7d430178432abb8
Abner Silva <abner.computadores@outlook.com>
2021-06-05 17:05:59 -0300

    add navigation para navegação entre telas

--- app/src/main/java/br/com/altechno/sbmovies/MainActivity.kt
+++ app/src/main/java/br/com/altechno/sbmovies/MainActivity.kt
@@ -2,10 +2,25 @@ package br.com.altechno.sbmovies
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navController = findNavController(R.id.nav_host_fragment)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
