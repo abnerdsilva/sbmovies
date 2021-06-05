989b173e22abc596b0905b677634d01ced38797b
Abner Silva <abner.computadores@outlook.com>
2021-06-05 18:02:13 -0300

    exibe itens no gridView de pesquisa

--- app/src/main/java/br/com/altechno/sbmovies/ui/search/SearchFragment.kt
+++ app/src/main/java/br/com/altechno/sbmovies/ui/search/SearchFragment.kt
@@ -5,9 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.GridView
 import android.widget.ImageView
 import androidx.navigation.fragment.findNavController
 import br.com.altechno.sbmovies.R
+import br.com.altechno.sbmovies.model.MovieSearch
+import br.com.mrstecno.mymoviesimdb_test.adapters.MoviesGridAdapter
 
 class SearchFragment : Fragment() {
 
@@ -27,5 +30,86 @@ class SearchFragment : Fragment() {
         backButton.setOnClickListener {
             findNavController().navigateUp()
         }
+
+        setupRecyclerViewMovies()
     }
+
+    private fun setupRecyclerViewMovies() {
+        val gridView: GridView = view?.findViewById(R.id.grid_view_movies)!!
+
+        gridView.adapter = MoviesGridAdapter(movies, requireContext()) {}
+    }
+
+    private val movies = listOf(
+        MovieSearch(
+            title = "Person",
+            poster = "https://www.pixsy.com/wp-content/uploads/2021/04/ben-sweet-2LowviVHZ-E-unsplash-1.jpeg",
+            imdbID = "",
+            type = "",
+            year = ""
+        ),
+        MovieSearch(
+            title = "Star Wars",
+            poster = "https://miro.medium.com/max/1838/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg",
+            imdbID = "",
+            type = "",
+            year = ""
+        ),
+        MovieSearch(
+            title = "Butterfly",
+            poster = "https://helpx.adobe.com/content/dam/help/en/stock/how-to/visual-reverse-image-search/jcr_content/main-pars/image/visual-reverse-image-search-v2_intro.jpg",
+            imdbID = "",
+            type = "",
+            year = ""
+        ),
+        MovieSearch(
+            title = "Birds",
+            poster = "https://ep00.epimg.net/verne/imagenes/2019/11/13/album/1573641411_551713_1573641467_album_normal.jpg",
+            imdbID = "",
+            type = "",
+            year = ""
+        ),
+        MovieSearch(
+            title = "Star Wars",
+            poster = "https://miro.medium.com/max/1838/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg",
+            imdbID = "",
+            type = "",
+            year = ""
+        ),
+        MovieSearch(
+            title = "Butterfly",
+            poster = "https://helpx.adobe.com/content/dam/help/en/stock/how-to/visual-reverse-image-search/jcr_content/main-pars/image/visual-reverse-image-search-v2_intro.jpg",
+            imdbID = "",
+            type = "",
+            year = ""
+        ),
+        MovieSearch(
+            title = "Birds",
+            poster = "https://ep00.epimg.net/verne/imagenes/2019/11/13/album/1573641411_551713_1573641467_album_normal.jpg",
+            imdbID = "",
+            type = "",
+            year = ""
+        ),
+        MovieSearch(
+            title = "Star Wars",
+            poster = "https://miro.medium.com/max/1838/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg",
+            imdbID = "",
+            type = "",
+            year = ""
+        ),
+        MovieSearch(
+            title = "Butterfly",
+            poster = "https://helpx.adobe.com/content/dam/help/en/stock/how-to/visual-reverse-image-search/jcr_content/main-pars/image/visual-reverse-image-search-v2_intro.jpg",
+            imdbID = "",
+            type = "",
+            year = ""
+        ),
+        MovieSearch(
+            title = "Birds",
+            poster = "https://ep00.epimg.net/verne/imagenes/2019/11/13/album/1573641411_551713_1573641467_album_normal.jpg",
+            imdbID = "",
+            type = "",
+            year = ""
+        )
+    )
 }
