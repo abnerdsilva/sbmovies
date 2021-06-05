e895b9d968fc61c1a3c5f37eb7c11159e2bdcdc7
Abner Silva <abner.computadores@outlook.com>
2021-06-05 19:04:44 -0300

    refactor MovieSearch para uso de Parcelable

--- app/src/main/java/br/com/altechno/sbmovies/model/MovieSearch.kt
+++ app/src/main/java/br/com/altechno/sbmovies/model/MovieSearch.kt
@@ -1,9 +1,44 @@
 package br.com.altechno.sbmovies.model
 
-data class MovieSearch(
-    val title: String,
-    val year: String,
-    val imdbID: String,
-    val type: String,
-    val poster: String,
-)
+import android.os.Parcel
+import android.os.Parcelable
+
+class MovieSearch(
+    val title: String?,
+    val year: String?,
+    val imdbID: String?,
+    val type: String?,
+    val poster: String?,
+) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString()
+    ) {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(title)
+        parcel.writeString(year)
+        parcel.writeString(imdbID)
+        parcel.writeString(type)
+        parcel.writeString(poster)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<MovieSearch> {
+        override fun createFromParcel(parcel: Parcel): MovieSearch {
+            return MovieSearch(parcel)
+        }
+
+        override fun newArray(size: Int): Array<MovieSearch?> {
+            return arrayOfNulls(size)
+        }
+    }
+
+}
