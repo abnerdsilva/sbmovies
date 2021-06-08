a1a2e1f7d0b4852e0efe72582ebae04de7a3d4f5
Abner Silva <abner.computadores@outlook.com>
2021-06-08 11:55:36 -0300

    pesquisa de detalhes do filme com viewModel e exibição dos dados

--- app/src/main/java/br/com/altechno/sbmovies/ui/details/DetailsFragment.kt
+++ app/src/main/java/br/com/altechno/sbmovies/ui/details/DetailsFragment.kt
@@ -1,10 +1,13 @@
 package br.com.altechno.sbmovies.ui.details
 
+import android.app.Dialog
 import android.os.Bundle
 import android.view.View
+import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import br.com.altechno.sbmovies.R
 import br.com.altechno.sbmovies.model.MovieSearch
@@ -12,6 +15,9 @@ import com.bumptech.glide.Glide
 
 class DetailsFragment : Fragment(R.layout.fragment_details) {
 
+    private val detailsViewModel: DetailsViewModel by viewModels()
+
+    private lateinit var dialog: Dialog
     private lateinit var movieSearch: MovieSearch
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,22 +32,78 @@ class DetailsFragment : Fragment(R.layout.fragment_details) {
         super.onViewCreated(view, savedInstanceState)
 
         val backButton: ImageView = view.findViewById(R.id.back_button)
-        val poster = view.findViewById<ImageView>(R.id.img_details_poster)
-        val txtTitle = view.findViewById<TextView>(R.id.txt_details_title)
-        val txtYear = view.findViewById<TextView>(R.id.txt_details_year)
-        val txtNoteImdb = view.findViewById<TextView>(R.id.txt_details_note_imdb)
-
-        txtTitle.text = movieSearch.Title
-        txtYear.text = movieSearch.Year
-        txtNoteImdb.text = movieSearch.imdbID
-
-        Glide.with(view)
-            .load(movieSearch.Poster)
-            .centerInside()
-            .into(poster)
 
         backButton.setOnClickListener {
             findNavController().navigateUp()
+        }
+
+        observerViewModels()
+    }
+
+    private fun observerViewModels() {
+        detailsViewModel.moviesLiveData.observe(viewLifecycleOwner, { movie ->
+            val poster = requireView().findViewById<ImageView>(R.id.img_details_poster)
+            val txtTitle = requireView().findViewById<TextView>(R.id.txt_details_title)
+            val txtYear = requireView().findViewById<TextView>(R.id.txt_details_year)
+            val txtNoteImdb = requireView().findViewById<TextView>(R.id.txt_details_note_imdb)
+            val txtGenre = requireView().findViewById<TextView>(R.id.txt_details_genre)
+            val txtSinopse = requireView().findViewById<TextView>(R.id.txt_details_sinopse)
+            val txtRuntime = requireView().findViewById<TextView>(R.id.txt_details_runtime)
+            val txtLanguage = requireView().findViewById<TextView>(R.id.txt_details_language)
+            val txtActors = requireView().findViewById<TextView>(R.id.txt_details_actors)
+
+            if (movie.Runtime == "N/A") {
+                txtRuntime.text = ""
+            }
+
+            txtTitle.text = movie.Title
+            txtYear.text = movie.Year.replace("–", "")
+            txtNoteImdb.text = "IMDb ${movie.imdbRating}"
+            txtSinopse.text = movie.Plot
+            txtGenre.text = movie.Genre
+            txtLanguage.text = movie.Language
+            txtActors.text = "Atores: ${movie.Actors}"
+
+            Glide.with(requireView())
+                .load(movie.Poster)
+                .centerInside()
+                .into(poster)
+        })
+
+        detailsViewModel.getIsLoading().observeForever { status ->
+            showLoading(status)
+        }
+
+        detailsViewModel.getMessage().observeForever { message ->
+            showMessageAlert(message)
+        }
+
+        movieSearch.Title?.let { detailsViewModel.findMovie(it) }
+    }
+
+    private fun showLoading(statusLoading: Boolean) {
+        if (statusLoading) {
+            dialog = Dialog(requireContext())
+            if (!dialog.isShowing) {
+                dialog.setContentView(R.layout.loading)
+                dialog.window!!.setLayout(
+                    ViewGroup.LayoutParams.WRAP_CONTENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT
+                )
+                dialog.show()
+            }
+        } else {
+            if (dialog.isShowing) {
+                dialog.dismiss()
+            }
+        }
+    }
+
+    private fun showMessageAlert(msg: String) {
+        view?.let {
+            val txtMessage: TextView = requireView().findViewById(R.id.txt_details_title)!!
+
+            txtMessage.text = msg
         }
     }
 }
---
+++ app/src/main/java/br/com/altechno/sbmovies/ui/details/DetailsViewModel.kt
@@ -0,0 +1,77 @@
+package br.com.altechno.sbmovies.ui.details
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import br.com.altechno.sbmovies.model.Movie
+import br.com.altechno.sbmovies.utils.RetrofitInitializer
+import br.com.altechno.sbmovies.utils.appdotenv
+import kotlinx.coroutines.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class DetailsViewModel: ViewModel() {
+
+    val moviesLiveData: MutableLiveData<Movie> = MutableLiveData()
+
+    private val isLoading = MutableLiveData<Boolean>()
+    private val message = MutableLiveData<String>()
+
+    fun findMovie(title: String) {
+        isLoading.value = true
+        message.value = ""
+
+        try {
+            CoroutineScope(Dispatchers.IO).launch {
+                val apiKey = appdotenv["SECRET_KEY"]
+
+                val response = async {
+                    RetrofitInitializer().movieService()
+                        .findMovie(apiKey, title)
+                }
+                val list = response.await()
+
+                withContext(Dispatchers.Main) {
+                    list.enqueue(object : Callback<Movie> {
+                        override fun onFailure(call: Call<Movie>, t: Throwable) {
+                            Log.e("ERROR", t.message.toString())
+                            message.value = t.message
+                            isLoading.value = false
+                        }
+
+                        override fun onResponse(
+                            call: Call<Movie>,
+                            response: Response<Movie>
+                        ) {
+                            response.body()?.let { rs ->
+                                if (rs.Response == "True") {
+                                    moviesLiveData.value = rs
+                                }
+                            }
+
+                            response.errorBody()?.let {
+                                message.value = response.message()
+                            }
+
+                            isLoading.value = false
+                        }
+                    })
+                }
+            }
+        } catch (e: Exception) {
+            Log.e("ERROR", e.message.toString())
+            message.value = e.message
+            isLoading.value = false
+        }
+    }
+
+    fun getMessage(): LiveData<String> {
+        return message
+    }
+
+    fun getIsLoading(): LiveData<Boolean> {
+        return isLoading
+    }
+}
